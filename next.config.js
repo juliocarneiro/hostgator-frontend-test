51fd40eccf31ef541d3596a1bfe2ea8cbcd904e9
juliocarneiro <juliocarneiro@gmail.com>
2020-07-27 00:18:52 -0300

    fix: netlify functions

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
+  target: 'serverless',
   pwa: {
     dest: 'public',
     disable: !isProd
